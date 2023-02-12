8ca98269acb41f6100ebfc32ae6f51dc02d63f00
xtombrock <xtombrock1989@gmail.com>
2023-02-12 17:44:08 +0000

    tailwind init

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
-
-
 function App() {
   return (
-    <>Hi</>
+    <>
+      <div className='isolate bg-black'>
+        <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      </div>
+    </>
   );
 }
 
