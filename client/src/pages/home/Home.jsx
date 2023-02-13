3851b499c07b23388ea46483959acdcda1093606
xtombrock <xtombrock1989@gmail.com>
2023-02-13 21:40:46 +0000

    hero basic init

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Header from '../../components/header/Header';
 import Navbar from '../../components/nav/Navbar';
 
 function Home() {
   return (
     <>
       <Navbar />
-      <div className='bg-blue-500'>Home</div>
+      <Header />
     </>
   )
 }
