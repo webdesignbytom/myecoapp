8aafa3db10b608945261bd6f702a98bd0635ae85
xtombrock <xtombrock1989@gmail.com>
2023-02-14 11:42:21 +0000

    coverted button to links

--- client/src/components/header/CTA.jsx
+++ client/src/components/header/CTA.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 function CTA() {
   return (
     <div className='grid grid-flow-col space-x-0 w-full gap-4'>
-      <button className='bg-eco-green border-2 rounded-md px-4 py-2 text-white hover:text-eco-green-dark hover:bg-white hover:border-eco-green'>
+      <Link className='bg-eco-green border-2 rounded-md px-4 py-2 text-white hover:text-eco-green-dark hover:bg-white hover:border-eco-green'>
         See More
-      </button>
-      <button className='border-2 rounded-md text-eco-green-dark border-eco-green hover:bg-eco-green px-4 py-2 hover:text-white hover:border-eco-green-dark'>
+      </Link>
+      <Link className='border-2 rounded-md text-eco-green-dark border-eco-green hover:bg-eco-green px-4 py-2 hover:text-white hover:border-eco-green-dark'>
         Projects
-      </button>
-      <button className='border-2 rounded-md text-eco-green-dark border-eco-green hover:bg-eco-green px-4 py-2 hover:text-white hover:border-eco-green-dark'>
+      </Link>
+      <Link className='border-2 rounded-md text-eco-green-dark border-eco-green hover:bg-eco-green px-4 py-2 hover:text-white hover:border-eco-green-dark'>
         Goals
-      </button>
+      </Link>
     </div>
   );
 }
